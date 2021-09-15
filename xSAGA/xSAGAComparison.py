2c74d4bf2be62e287e8e14e4f859006be33e38da
jdvannest (macbook) <jordanvannest@gmail.com>
2021-09-15 14:18:40 -0500

    Added directory for xSAGA collaboration with John Wu

---
+++ xSAGA/xSAGAComparison.py
@@ -0,0 +1,25 @@
+import pickle,argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--definition", choices=['1','2','3','4'],required=True)
+parser.add_argument("-r", "--radius",choices=['sim','300'],required=True)
+args = parser.parse_args()
+
+sats = pickle.load(open(f'Satellite.{args.definition}.{args.radius}.pickle','rb'))
+host = pickle.load(open(f'MilkyWay.{args.definition}.{args.radius}.pickle','rb'))
+
+out = open(f'xSAGAComparison.{args.definition}.{args.radius}.txt','w')
+out.writelines(f'HostID\tHost_Mstar\tSatID\tSat_Mstar\tSat_pos\n')
+
+for mw in host:
+    mw_id = int(mw)
+    mw_cen = host[mw]['center']
+    mw_ms = host[mw]['Mstar']
+    for s in host[mw]['Satellites']:
+        sat_id = int(s)
+        sat_cen = sats[s]['center']
+        sat_ms = sats[s]['Mstar']
+
+        out.writelines(f'{mw_id}\t{mw_ms}\t{sat_id}\t{sat_ms}\t{sat_cen-mw_cen}\n')
+
+out.close()
