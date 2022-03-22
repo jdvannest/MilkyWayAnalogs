ce8aaf77321d5e53b026c16cfa3c81209210233b
jdvannest (macbook) <jordanvannest@gmail.com>
2022-03-22 16:34:29 -0500

    Updated broad Mvir def to have upper bound so groups aren't pulled

--- MilkyWayAnalogs.py
+++ MilkyWayAnalogs.py
@@ -75,10 +75,10 @@ myprint(f'{len(LargeHalos)} Large Halos Found',clear=True)
 print('Searching for Milky Way Analogs...')
 if args.definition == '1':
     criteria = mvir
-    ##previous (lb,ub) values: (1e11,-),(5e11,3e12),(1e12,4e12)
+    ##previous (lb,ub) values: (1e11,1e12.6),(5e11,3e12),(1e12,4e12)
     ##                          1.08e12 +/- 15% from https://arxiv.org/abs/2111.09327                          
     lower_bound = 1e11 
-    upper_bound = 1e14
+    upper_bound = 10**12.6
 elif args.definition in ['5','6','7']:
     criteria = kmag
     lower_bound = -24.6
