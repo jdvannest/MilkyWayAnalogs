91f38492b314785c022e81ea21fa42b4e45c874d
jdvannest (glaurung) <jordanvannest@gmail.com>
2022-03-23 10:38:57 -0500

    Added UnderMassiveHistory Data

--- OtherCode/UnderMassiveHistory.py
+++ OtherCode/UnderMassiveHistory.py
@@ -2,10 +2,10 @@ import os,pickle
 
 halos = [312,212,227,226,217,254,280,205,338,235]
 
-print('Loading Database...')
 os.environ['TANGOS_DB_CONNECTION'] = '/myhome2/users/munshi/Romulus/data_romulus25.working.db'
 import tangos
 rom = tangos.get_simulation('cosmo25')[-1]
+print('Database Loaded')
 
 Data={}
 
@@ -19,3 +19,4 @@ for h in halos:
 out = open('Data/UnderMassiveHistoryData.pickle','wb')
 pickle.dump(Data,out)
 out.close()
+print('Data Written')
