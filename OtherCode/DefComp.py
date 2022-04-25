40d912b2594bd229452fd0e96b48e4f22acf13f7
jdvannest (macbook) <jordanvannest@gmail.com>
2022-04-25 12:46:58 -0500

    Added comparison plots for M* and Mk defs and updated plots for defs 5,6,7

--- OtherCode/DefComp.py
+++ OtherCode/DefComp.py
@@ -35,3 +35,33 @@ cbar.ax.tick_params(labelsize=13)
 ax.scatter(mv1,ms1,facecolor='None',edgecolor='r',s=7.2**2,label=r'M$_{vir}$')
 ax.legend(loc='upper left',prop={'size':15})
 f.savefig('Data/DefComp.png',bbox_inches='tight',pad_inches=.1)
+
+
+
+M1 = pickle.load(open('../DataFiles/MilkyWay.2.sim.pickle','rb'))
+M2 = pickle.load(open('../DataFiles/MilkyWay.5.sim.pickle','rb'))
+
+ms1,ms2,km1,km2 = [[],[],[],[]]
+
+for mw in M1:
+    ms1.append(np.log10(M1[mw]['Mstar']))
+    km1.append(M1[mw]['Kmag']-1.85)
+for mw in M2:
+    ms2.append(np.log10(M2[mw]['Mstar']))
+    km2.append(M2[mw]['Kmag'])
+
+f,ax = plt.subplots(1,1,figsize=(6,6))
+ax.set_xlim([-25,-22])
+ax.set_ylim([9.75,11.25])
+ax.plot([-26,-21],[10,10],c='k',linestyle='--',alpha=.5)
+ax.plot([-26,-21],[11,11],c='k',linestyle='--',alpha=.5)
+ax.axvline(-24.6,c='k',linestyle='--',alpha=.5)
+ax.axvline(-23,c='k',linestyle='--',alpha=.5)
+ax.set_xlabel(r'M$_{K}$',fontsize=20)
+ax.set_ylabel(r'Log(M$_*$)',fontsize=20)
+ax.tick_params(labelsize=13)
+
+ax.scatter(km2,ms2,s=6.8**2,c='k',cmap='viridis',label=r'$-24.6<$M$_K<-23$')
+ax.scatter(km1,ms1,facecolor='None',edgecolor='r',s=7.2**2,linewidth=2,label=r'$10^{10}<$Log(M$_{*}$)$<10^{11}$')
+ax.legend(loc='upper right',prop={'size':15})
+f.savefig('Data/DefComp2.png',bbox_inches='tight',pad_inches=.1)
---
+++ OtherCode/KmagComp.py
@@ -0,0 +1,49 @@
+import pickle
+import numpy as np
+import matplotlib.pylab as plt
+import matplotlib.patches as patches
+
+k = pickle.load(open('../DataFiles/MilkyWay.5.sim.pickle','rb'))
+st = pickle.load(open('../DataFiles/MilkyWay.2.sim.pickle','rb'))
+
+mv1,km1 = [[],[]]
+mv2,km2 = [[],[]]
+
+for m in k:
+    mv2.append(np.log10(k[m]['Mvir']))
+    km2.append(k[m]['Kmag'])
+for m in st:
+    mv1.append(np.log10(st[m]['Mvir']))
+    km1.append(st[m]['Kmag']-1.85)
+
+ux,uy,lx,ly = [[],[],[],[]]
+with open('KmagSAGABounds.csv') as f:
+    lines = f.readlines()
+for line in lines:
+    l = line.split(',')
+    if l[0]!='':
+        ux.append(-1*float(l[0][1:]))
+        uy.append(float(l[1]))
+    if l[2]!='':
+        lx.append(-1*float(l[2][1:]))
+        ly.append(float(l[3]))
+
+u = np.polyfit(ux,uy,2)
+x1 = np.linspace(-24.62,-22,100)
+l = np.polyfit(lx,ly,2)
+x2 = np.linspace(-25.09,-22,100)
+
+
+f,ax = plt.subplots(1,1)
+rect = patches.Rectangle((-25.1,11.4),3.1,1.6,linestyle='--',edgecolor='0.5',facecolor='none')
+ax.add_patch(rect)
+ax.plot(x1,u[0]*x1**2+u[1]*x1+u[2],c='0.5',label='SAGA-I Fig. 2')
+ax.plot(x2,l[0]*x2**2+l[1]*x2+l[2],c='0.5')
+ax.scatter(km1,mv1,s=5**2,c='k',label=r'$10^{10}<$Log(M$_{*}$)$<10^{11}$')
+ax.scatter(km2,mv2,s=6**2,edgecolor='r',facecolor='None',linewidth=1.3,label=r'$-24.6<$M$_K<-23$')
+ax.set_ylim([10.9,13.2])
+ax.set_xlim([-26,-21])
+ax.set_xlabel(r'M$_K$',fontsize=15)
+ax.set_ylabel(r'Log(M$_{vir}$/M$_\odot$)',fontsize=15)
+ax.legend(loc='upper right',prop={'size':13})
+f.savefig('Data/KmagComp.png',bbox_inches='tight',pad_inches=.1)
